80c8691b97e9a63c1a46a77f5eeb512ebe437ad3
e-danziger <eduard.danziger@googlemail.com>
2025-08-03 00:06:01 +0200

    ApiDll project added

---
+++ Projects/SoundAgentApiDll/SoundAgentApi.cpp
@@ -0,0 +1,90 @@
+﻿#include "stdafx.h"
+
+#include "SoundAgentApi.h"
+
+#include "public/SoundAgentInterface.h"
+#include "ApiClient/common/ClassDefHelper.h"
+
+class DllObserver final : public SoundDeviceObserverInterface {
+public:
+    explicit DllObserver(TSaaDiscover discoverCallback)
+        : discoverCallback_(discoverCallback)
+    {
+    }
+    DISALLOW_COPY_MOVE(DllObserver);
+    ~DllObserver() override;
+
+    void OnCollectionChanged(SoundDeviceEventType event, const std::string& devicePnpId) override;
+
+private:
+    TSaaDiscover discoverCallback_;
+};
+
+DllObserver::~DllObserver() = default;
+
+void DllObserver::OnCollectionChanged(SoundDeviceEventType event, const std::string& devicePnpId)
+{
+    if(discoverCallback_ != nullptr)
+    {
+        const bool attached = event == SoundDeviceEventType::Discovered;
+        // ReSharper disable once CppTooWideScopeInitStatement
+        const bool detached = event == SoundDeviceEventType::Detached;
+
+        if (attached || detached)
+        {
+            // SaaDescription description; // only for logging
+            // strncpy_s(description.Guid, _countof(description.Guid), device->GetPnpId().c_str(), device->GetPnpId().size());
+            // strncpy_s(description.Name, _countof(description.Name), device->GetName().c_str(), device->GetName().size());
+            // description.Volume = 100;
+            //
+            discoverCallback_(attached ? TRUE : FALSE);
+        }
+    }
+}
+
+
+namespace  {
+    std::unique_ptr< SoundDeviceCollectionInterface> device_collection;
+    std::unique_ptr<SoundDeviceObserverInterface> device_collection_observer;
+}
+
+SaaResult AcInitialize(SaaHandle* handle, TSaaDiscover discoverCallback)
+{
+    device_collection = SoundAgent::CreateDeviceCollection();
+    device_collection_observer = std::make_unique<DllObserver>(discoverCallback);
+    device_collection->Subscribe(*device_collection_observer);
+
+    return 0;
+}
+
+SaaResult AcGetAttached(SaaHandle handle, SaaDescription* description)
+{
+    if(description == nullptr)
+    {
+        return 0;
+    }
+
+    if (device_collection != nullptr && device_collection->GetSize() > 0)
+    {
+        const auto device = device_collection->CreateItem(0);
+        strncpy_s(description->Guid, _countof(description->Guid), device->GetPnpId().c_str(), device->GetPnpId().size());
+        strncpy_s(description->Name, _countof(description->Name), device->GetName().c_str(), device->GetName().size());
+        description->Volume = device->GetCurrentRenderVolume();
+        return 0;
+    }
+    description->Guid[0] = '\0';
+    description->Name[0] = '\0';
+    description->Volume = 0;
+    return 0;
+}
+
+SaaResult AcUnInitialize(SaaHandle handle)
+{
+    if(device_collection != nullptr)
+    {
+        device_collection->Unsubscribe(*device_collection_observer);
+        device_collection_observer.reset();
+        device_collection.reset();
+    }
+    return 0;
+}
---
+++ Projects/SoundAgentApiDll/SoundAgentApi.h
@@ -0,0 +1,55 @@
+﻿#ifndef SOUND_AGENT_API_H
+#define SOUND_AGENT_API_H
+
+#include <Windows.h>
+
+#ifdef ED_EXPORTS
+#define SAA_EXPORT_IMPORT_DECL __declspec(dllexport)
+#else
+#define AC_EXPORT_IMPORT_DECL __declspec(dllimport)
+#endif
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+    /**
+     * @typedef AcHandle
+     * @brief Handle type used to identify a specific audio control session.
+     */
+    typedef DWORD64 SaaHandle;
+
+    typedef INT32 SaaResult;
+
+    typedef struct {
+        CHAR Guid[40];
+        CHAR Name[128];
+        UINT16 Volume;
+    } SaaDescription;
+
+    typedef void(__stdcall* TSaaDiscover)(
+        _In_  BOOL  attach
+        );
+
+    SAA_EXPORT_IMPORT_DECL
+        SaaResult __stdcall AcInitialize(
+        _Out_ SaaHandle* handle,
+        _In_opt_ TSaaDiscover discoverCallback
+    );
+
+    SAA_EXPORT_IMPORT_DECL
+        SaaResult __stdcall AcGetAttached(
+            _In_ SaaHandle handle,
+            _Out_  SaaDescription* description
+        );
+
+    SAA_EXPORT_IMPORT_DECL
+        SaaResult __stdcall AcUnInitialize(
+            _In_ SaaHandle handle
+        );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SOUND_AGENT_API_H
